65024ebd28c2f2e8aa6df1aaa4d0632e09201763
keelykirkpatrick <44475218+keelykirkpatrick@users.noreply.github.com>
2018-11-12 22:40:33 -0500

    Add files via upload

--- pathfinder.py
+++ pathfinder.py
@@ -1,12 +1,16 @@
-# from PIL import Image
-# im = Image.open("samples/map_with_path.png")
-# im.rotate(45).show()
+#Goal: Using obj oriented programming and testing, build a program to read elevation data
+#draw an elevation map as a .png file
+#chart the best path across the map
+
+from PIL import Image
+img = Image.open("samples/map_alone.png")
+img.rotate(45).show()
+
 
 with open("elevation_small.txt") as text_file:
     keelylist = []
     for line in text_file.readlines():
         for item in line:
             keelylist.append([int(item) for item in line.strip().split()])
-            print(keelylist)
-
-
+        print(keelylist)
+        
