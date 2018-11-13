8deeb0fc03ef6d9a8f0e7a7f5d7222321fe0cae5
keelykirkpatrick <44475218+keelykirkpatrick@users.noreply.github.com>
2018-11-13 01:45:36 -0500

    Add files via upload
    Class updates and added self/defs

--- pathfinder.py
+++ pathfinder.py
@@ -3,9 +3,7 @@
 #chart the best path across the map
 
 from PIL import Image
-img = Image.open("samples/map_alone.png")
-img.rotate(45).show()
-
+import random
 
 with open("elevation_small.txt") as text_file:
     keelylist = []
@@ -13,4 +11,40 @@ with open("elevation_small.txt") as text_file:
         for item in line:
             keelylist.append([int(item) for item in line.strip().split()])
         print(keelylist)
-        
+
+#Elevation Map Class 
+class ElevationMap:
+    def ___init___(self, data_file = None):
+        if data_file is None:
+            self.data = [ [4750, 4740, 4701, 4696],
+                        [  4714, 4708, 4698, 4691],
+                        [  4719, 4709, 4706, 4702],
+                        [  4720, 4726, 4708, 4701]]
+        self.data = []
+        with open(data_file) as new_file:
+            for row in new_file: 
+                lines = row.split()
+                lines = [int(item) for item in lines]
+                self.data.append(lines) 
+
+    def get_min(self) to int:
+        return min([min(row) for row in self.data])
+
+    def get_max(self) to int:
+        return max([max(row) for row in self])
+
+
+#Map Image class to draw
+class MapImage:
+    def ___init___(self, map_data, pathfinder):
+        self.map_data = map_data
+        self.pathfinder = pathfinder
+        self.height = len(self.map_data.data)
+        self.width = len(self.map_data.data[0])
+        self.show = Image.new("RGBA", (self.width, self.height))
+
+    def DrawImg(self, file_name):
+
+    def DrawPath(self, file_name, color):
+
+    
